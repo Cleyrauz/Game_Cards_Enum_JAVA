3d0d25c4acaaa89eaf2174040a7299ff637a85ef
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-06-20 18:16:03 +0100

    Added RankType, SuitType and getters + constructor in Card Class

--- Game/src/main/java/Card.java
+++ Game/src/main/java/Card.java
@@ -1,2 +1,20 @@
 public class Card {
+
+    private SuitsType suits;
+    private RankType rank;
+
+    public Card(SuitsType suits, RankType rank) {
+        this.suits = suits;
+        this.rank = rank;
+    }
+
+    public SuitsType getSuits() {
+        return suits;
+    }
+
+    public RankType getRank() {
+        return rank;
+    }
+
+
 }
