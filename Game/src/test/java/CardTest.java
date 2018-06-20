e20b81a033371639be972d95df927512de350860
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-06-20 18:46:05 +0100

    Added Card Tests

--- Game/src/test/java/CardTest.java
+++ Game/src/test/java/CardTest.java
@@ -1,3 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    private Card card;
+
+    @Before
+    public void before(){
+      card = new Card(SuitsType.CLUBS, RankType.EIGHT);
+    }
+
+    @Test
+    public void testCardHasSuit(){
+        assertEquals(SuitsType.CLUBS, this.card.getSuits());
+    }
+
+    @Test
+    public void testCardHasRank(){
+        assertEquals(RankType.EIGHT, this.card.getRank());
+    }
+
+    @Test
+    public void testCardHasValue() {
+        assertEquals(8, this.card.getValueFromEnum());
+    }
+
+    @Test
+    public void testCardHasAName(){
+        assertEquals("EIGHT of CLUBS", this.card.niceName());
+    }
     
 }
